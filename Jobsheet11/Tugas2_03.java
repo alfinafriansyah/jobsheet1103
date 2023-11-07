476e11ce95ccae3c70c1ac68fbef310cfc15428e
Alfin Afriansyah <143513533+alfinafriansyah@users.noreply.github.com>
2023-11-07 14:08:22 +0700

    Add files via upload

---
+++ Jobsheet11/Tugas2_03.java
@@ -0,0 +1,18 @@
+package Jobsheet11;
+
+import java.util.Scanner;
+
+public class Tugas2_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
